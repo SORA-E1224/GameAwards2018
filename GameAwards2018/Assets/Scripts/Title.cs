5ec253150aacfda05011aef0bd653ac4d228cb30
Marinka <maruta3150@yahoo.co.jp>
2018-06-25 14:30:18 +0900

    Merge branch 'Ise' of https://github.com/SORA-E1224/GameAwards2018 into Ise

---
+++ GameAwards2018/Assets/Scripts/Title.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Title : MonoBehaviour
+{
+    [SerializeField]
+    private Animator target;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneController.mode == SceneController.FADE_MODE.FADEMODE_NONE) {
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            target.SetTrigger("character_nearby");
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            target.ResetTrigger("character_nearby");
+        }
+
+    }
+}
---
+++ GameAwards2018/Assets/Scripts/TitleLight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleLight : MonoBehaviour {
+    [SerializeField]
+    private Light target;
+    [SerializeField]
+    private AnimationCurve rangeCurve;
+    private float i;
+	// Use this for initialization
+	void Start () {
+        i = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        i += Time.deltaTime;
+        target.range = rangeCurve.Evaluate(i);
+	}
+}
